eda916d9878c5845cd790a7a4346f6e135338297
MaxwellZen <maxwell.zen@gmail.com>
2021-01-13 13:45:10 -0500

    did optimizations

--- Radix.java
+++ Radix.java
@@ -22,12 +22,18 @@ public class Radix {
           int passes = 0;
           SortableLinkedList buckets[] = new SortableLinkedList[10];
           for (int i = 0; i < 10; i++) buckets[i] = new SortableLinkedList();
-          for (int i = 0; i < data.size(); i++) {
+          int s = data.size();
+          int k;
+          while (s-->0) {
                passes = Math.max(passes, length(data.get(0)));
                data.add(data.remove(0));
           }
           for (int digit = 0; digit < passes; digit++) {
-               while (data.size()>0) buckets[nth(data.get(0), digit)].add(data.remove(0));
+               s = data.size();
+               while (s-->0) {
+                    k = data.remove(0);
+                    buckets[nth(k, digit)].add(k);
+               }
                merge(data, buckets);
           }
      }
@@ -36,12 +42,18 @@ public class Radix {
           int passes = 0;
           SortableLinkedList buckets[] = new SortableLinkedList[19];
           for (int i = 0; i < 19; i++) buckets[i] = new SortableLinkedList();
-          for (int i = 0; i < data.size(); i++) {
+          int s = data.size();
+          int k;
+          while (s-->0) {
                passes = Math.max(passes, length(data.get(0)));
                data.add(data.remove(0));
           }
           for (int digit = 0; digit < passes; digit++) {
-               while (data.size()>0) buckets[nth(data.get(0), digit)+9].add(data.remove(0));
+               s = data.size();
+               while (s-->0) {
+                    k = data.remove(0);
+                    buckets[nth(k, digit)+9].add(k);
+               }
                merge(data, buckets);
           }
      }
